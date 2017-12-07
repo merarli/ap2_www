ab617536aa23faa85725237d56038b62215010be
merarli <merarli@Merarli12.local>
2017-12-07 16:33:23 +0900

    少し安全にした

---
+++ src/java/pkg20171207/Kadai10_2.java
@@ -0,0 +1,179 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg20171207;
+
+//import java.beans.Statement;
+import pkg20171205.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import static java.lang.Character.UnicodeBlock.forName;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author merarli
+ */
+@WebServlet(name = "Kadai10_2", urlPatterns = {"/pkg20171207/Kadai10_2"})
+public class Kadai10_2 extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, ClassNotFoundException, SQLException {
+        response.setContentType("text/html;charset=UTF-8");
+
+        //コネクション、ステートメントの宣言
+        //コネクションとステートメントの宣言
+        Connection con = null;
+        Statement stmt = null;
+
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet Ex101</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h3>Servlet Ex101 at " + request.getContextPath() + "</h3>");
+
+            //Class.forNameの記述
+            Class.forName("org.apache.derby.jdbc.ClientDriver");
+
+            //データベースへの接続
+            String driverUrl = "jdbc:derby://localhost:1527/twitter";
+            con = DriverManager.getConnection(driverUrl, "db", "db");
+            stmt = con.createStatement();
+
+            //SQL挿入
+//            String sql2 = "insert into T_STUDENT_A (fullname,gakubu_id,grade) values('SQLTEST_kun_2aaa',1,4)";
+//            int count;
+//            count = stmt.executeUpdate(sql2);
+            int id = Integer.parseInt(request.getParameter("te"));
+            
+            //SQLの発行
+            String sql = "select * from T_TWEET where TWEETNO=?";
+            
+            //ステートメントの作成
+            PreparedStatement ps = con.prepareStatement(sql);
+            
+            ps.setInt(1,id);
+            
+            //SQLを実行してResultSetの形式で結果を取得
+            ResultSet rs = ps.executeQuery();
+            
+            
+//            ResultSet rs = stmt.executeQuery(sql);
+            //データベースから取得
+            
+                //データベースから取得
+                while (rs.next()) {
+                    out.print("TWEETNO:" + rs.getInt("TWEETNO") + "<br>");
+                    out.print("TWITTERID:" + rs.getString("TWITTERID") + "<br>");
+                    out.print("TWEETTEXT:" + rs.getString("TWEETTEXT") + "<br>");
+                    out.print("IINE:" + rs.getString("IINE") + "<br>");
+                    out.print("<hr>");
+                }
+            
+
+            //ResultSetのclose
+            rs.close();
+            
+            out.println("</body>");
+            out.println("</html>");
+        } catch (Exception e) {
+            //サーブレット内での例外をアプリケーションのエラーとして表示
+            throw new ServletException(e);
+        } finally {
+            //例外が発生する・しないにかかわらず確実にデータベースから切断
+            if (stmt != null) {
+                try {
+                    stmt.close();
+                } catch (SQLException e) {
+                    throw new ServletException(e);
+                }
+            }
+            if (con != null) {
+                try {
+                    con.close();
+                } catch (SQLException e) {
+                    throw new ServletException(e);
+                }
+            }
+        }
+    }
+
+// <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Kadai10_2.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(Kadai10_2.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Kadai10_2.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(Kadai10_2.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
